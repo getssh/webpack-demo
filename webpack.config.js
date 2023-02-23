15d2e28211a43c0c0662be10a84ff42df8682961
getssh <gtamrat33@gmail.com>
2023-02-24 02:42:29 +0300

    Add webpack server dependecy and style css loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,30 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
-   entry: {
+  entry: {
      index: './src/index.js',
    },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-   output: {
+  output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist'),
    },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
  };
